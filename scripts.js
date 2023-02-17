0bbb6af02da328dcac62dcae3e8dca359608875a
Rogueware <hello@rogueware.in>
2023-02-17 08:23:20 +0530

    init commit

---
+++ scripts.js
@@ -0,0 +1,73 @@
+const QuestionsArr = 
+    [
+        "What is the value of x if x² + 2x + 1 = 0?",
+        "What is the value of x if x² - 4x + 4 = 0?",
+        "What is the value of x if x² - 3x - 4 = 0?",
+        "What is the value of x if 2x² - 7x + 3 = 0?",
+        "What is the value of x if x² + x - 2 = 0?",
+        "What is the value of x if x² - 6x + 9 = 0?",
+        "What is the value of x if x² + 6x + 9 = 0?",
+        "What is the value of x if x² - 5x + 6 = 0?",
+        "What is the value of x if x² - 8x + 16 = 0?",
+        "What is the value of x if x² - 10x + 25 = 0?",
+        "If you lend £500 at an 8% interest rate for 2 years, how much simple interest will you earn?",
+        "If you borrow £300 at a 6% interest rate for 1 year, how much simple interest will you need to pay back?",
+        "If you have £1000 and the interest rate is 5%, how much simple interest will you earn in 3 years?",
+        "If you have £100 and the interest rate is 5%, how much simple interest will you earn in one year?",
+        "If you have £200 and the interest rate is 4%, how much simple interest will you earn in two years?",
+        "If you lend £300 at a 5% interest rate for three years, how much simple interest will you earn?",
+        "If you borrow £500 at a 6% interest rate for four years, how much simple interest will you need to pay back?",
+        "If you have £1000 and the interest rate is 3%, how much simple interest will you earn in five years?",
+        "If you lend £2000 at a 4% interest rate for six years, what is the total amount you will earn?",
+        "If you invest £500 at an annual interest rate of 6%, what is the total amount you will receive?",
+        "Daniel Invests £50 at a rate of 3% compound interest for 5 years. How much money will Daniel have gained after 5 years? Round your answer to the nearest whole number.",
+    ]
+
+const AnswersArr = [
+    "-1",
+    "2",
+    "4",
+    "4",
+    "-2",
+    "3",
+    "-3",
+    "2",
+    "-4",
+    "5",
+    "80",
+    "18",
+    "150",
+    "5",
+    "16",
+    "45",
+    "120",
+    "150",
+    "2480",
+    "590",
+    "8",
+]
+
+const Question = document.getElementById("question");
+const Answer = document.getElementById("answer");
+const Submit = document.getElementById("submit");
+
+let num = 0;
+
+function Generate() {
+    num = Math.floor(Math.random() * QuestionsArr.length);
+    Question.innerHTML = QuestionsArr[num];
+    Answer.value = "";
+    Answer.focus();
+}
+
+function Check() {
+    Question.innerHTML = "(Press generate for another)";
+    if (Answer.value == AnswersArr[num]) {
+        alert("You're correct!");
+    } else {
+        alert("Incorrect! The correct answer was " + AnswersArr[num] + ". Skip a turn!");
+    }
+}
+
+document.getElementById("generate").onclick = Generate;
+document.getElementById("submit").onclick = Check;
